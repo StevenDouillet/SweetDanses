b957b02b9c83ebed5f42e5e2afb43959236cfdc1
DOUILLET <Steven.douillet@gmail.com>
2021-07-28 18:59:31 +0200

    complete and adjust prices page

--- resources/views/pages/prices.blade.php
+++ resources/views/pages/prices.blade.php
@@ -2,12 +2,17 @@
 
 @section('content')
 
-<div class="flex flex-col items-center justify-center mt-16 space-y-8 lg:flex-row lg:items-stretch lg:space-x-8 lg:space-y-0">
+<div class="container mx-auto prices-info-container">
+    <p>Vous pouvez directement vous inscrire en suivant les instructions sur le lien suivant : </p>
+    <a href="https://www.google.com">https://www.google.com</a>
+</div>
+
+<div class="flex flex-col items-center justify-center mt-14 space-y-8 lg:flex-row lg:items-stretch lg:space-x-8 lg:space-y-0">
     <section class="flex flex-col w-full max-w-sm p-12 space-y-6 bg-white rounded-lg shadow-md">
         <!-- Price -->
         <div class="flex-shrink-0">
-            <div class="text-3xl font-medium tracking-tight">199 euros</div>
-            <div class="text-gray-400">pour l'année entière</div>
+            <div class="text-3xl font-medium tracking-tight text-center">199 euros*</div>
+            <div class="text-gray-400 text-center">pour l'année entière</div>
         </div>
 
         <!--  -->
@@ -18,27 +23,23 @@
         <!-- Features -->
         <ul class="flex-1 space-y-4">
             <li class="flex items-start">
-                <span class="ml-3 text-base font-medium">269€ pour deux danses</span>
+                <span class="ml-3 text-base font-medium">269€ pour deux danses*</span>
             </li>
             <li class="flex items-start">
-                <span class="ml-3 text-base font-medium">Ensuite 30€ par cours supplémentaires</span>
-            </li>
-            <li class="flex items-start">
-                <span class="ml-3 text-base font-medium">Prévoir 19€ de cotisation FFD</span>
+                <span class="ml-3 text-base font-medium">Puis 30€ par cours supplémentaire</span>
             </li>
         </ul>
 
         <!-- Button -->
-        <div class="flex-shrink-0 pt-4">
-            <button
-                class="p-5 inline-flex items-center justify-center w-full max-w-xs px-4 py-2 rounded-full text-gray-100 bgc-pink">Demander un essai gratuit</button>
+        <div class="flex-shrink-0 pt-4 text-center">
+            <a href="#" class="small-sw-button">Demander un essai gratuit</a>
         </div>
     </section>
     <section class="flex flex-col w-full max-w-sm p-12 space-y-6 bg-white rounded-lg shadow-md">
         <!-- Price -->
         <div class="flex-shrink-0">
-            <div class="text-3xl font-medium tracking-tight">378 euros</div>
-            <div class="text-gray-400">pour l'année entière</div>
+            <div class="text-3xl font-medium tracking-tight text-center">378 euros*</div>
+            <div class="text-gray-400 text-center">pour l'année entière</div>
         </div>
 
         <!--  -->
@@ -49,22 +50,22 @@
         <!-- Features -->
         <ul class="flex-1 space-y-4">
             <li class="flex items-start">
-                <span class="ml-3 text-base font-medium">523€ pour deux danses</span>
+                <span class="ml-3 text-base font-medium">523€ pour deux danses*</span>
             </li>
             <li class="flex items-start">
-                <span class="ml-3 text-base font-medium">Ensuite 50€ par cours supplémentaires</span>
-            </li>
-            <li class="flex items-start">
-                <span class="ml-3 text-base font-medium">Prévoir 38€ de cotisation FFD</span>
+                <span class="ml-3 text-base font-medium">Puis 50€ par cours supplémentaire</span>
             </li>
         </ul>
 
         <!-- Button -->
-        <div class="flex-shrink-0 pt-4">
-            <button
-                class="p-5 inline-flex items-center justify-center w-full max-w-xs px-4 py-2 rounded-full text-gray-100 bgc-pink">Demander un essai gratuit</button>
+        <div class="flex-shrink-0 pt-4 text-center">
+            <a href="#" class="small-sw-button">Demander un essai gratuit</a>
         </div>
     </section>
+</div>
+
+<div class="container mx-auto prices-asterix-container">
+    <p>*Prix comprenant 10€ de cotisation à l'association et 19€ de cotisation FFD</p>
 </div>
 
 @endsection
